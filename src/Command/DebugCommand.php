a96713a67da007317cb3e88ac6f5e4c63afae996
Harold Juarez <hjuarez20@gmail.com>
2017-07-28 18:15:32 -0600

    [console] Extend command base

--- src/Command/DebugCommand.php
+++ src/Command/DebugCommand.php
@@ -4,8 +4,7 @@ namespace Drupal\Console\Dotenv\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Command\Command;
-use Drupal\Console\Core\Command\Shared\CommandTrait;
+use Drupal\Console\Core\Command\Command;
 use Drupal\Console\Core\Style\DrupalStyle;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -16,8 +15,6 @@ use Symfony\Component\Filesystem\Filesystem;
  */
 class DebugCommand extends Command
 {
-    use CommandTrait;
-
     /**
      * @var string
      */
